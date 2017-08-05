14ec32b4022ec3a859598bf6ddc82a1cb7e0644d
Arshdeep Singh <singh_arsy@yahoo.com>
2017-08-05 17:13:15 -0400

    add images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about_us
+  end
+
+  def our_fleet
+  end
+
+  def reservations
+  end
+
+  def billing
+  end
+
+  def contact_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  get "/about_us" => "home#about_us"
+  get "/contact_us" => "home#contact_us"
+  get "/our_fleet" => "home#our_fleet"
+  get "/reservations" => "home#reservations"
+  get "/billing" => "home#billing"
 end
